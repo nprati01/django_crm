0044cb81b20ff0f3f5e109c1c22e46f5a83912b1
nprati01 <nicole.e.prati@gmail.com>
2023-06-23 13:56:48 -0400

    login functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 # holds each route we define for main_app
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    # path('login/', views.login_user, name='login'),
+    # path('logout/', views.logout_user, name='logout'),
 
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -4,3 +4,9 @@ from django.contrib import messages
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def login_user(request):
+    pass
+
+def logout_user(request):
+    pass
